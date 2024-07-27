63f7dfc7c822d1a24ff0bff82203893556cdc0a3
Pradeep gaur <59308953+pradeep652227@users.noreply.github.com>
2024-07-27 11:46:24 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,10 @@
+"use strict";//by default it is enabled by the browsers (nowadays)
+//to use latest standards of js
+
+console.table([
+    typeof 12,
+    typeof BigInt,
+    typeof null,
+    typeof undefined,
+    typeof symbol
+]);
