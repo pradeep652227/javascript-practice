074a76e89162d98e4d1c40e5e600c3f7b7d1592e
Pradeep gaur <59308953+pradeep652227@users.noreply.github.com>
2024-07-29 17:19:13 +0000

    comparison-operators

---
+++ 01_basics/comparison-of-datatypes.js
@@ -0,0 +1,14 @@
+console.table([undefined >=0,undefined==0,undefined>0]);
+
+console.log("2"==2);
+console.log("2"===2);
+
+console.table(
+    [
+        "2">2,
+        "02"==2,
+        "02">=2
+    ]
+);
+
+//Equality operator work differently as compared to comparison operators
