d780a939669a1d5620fd4359f1cc279d922c3c01
Pradeep gaur <59308953+pradeep652227@users.noreply.github.com>
2024-08-05 16:58:25 +0000

    strings

---
+++ 01_basics/07_StringsinJS.js
@@ -0,0 +1,16 @@
+//String Interpolation
+
+let name="Pradeep";
+
+console.log(`My Name = ${name.toUpperCase()}.`);
+
+//String Declaration (Object);
+const str=new String("StringObj fsdf sdfdsf");
+
+//methods
+console.log(str.substring(1,4));
+console.log(str.slice(-str.length,4));
+
+console.log(str.split(' '));
+
+console.log(str.toLowerCase().includes('str'));
