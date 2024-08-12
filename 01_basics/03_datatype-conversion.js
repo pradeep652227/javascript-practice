84eea495114ed017e05ba05d1d167fbbe9df324b
Pradeep gaur <59308953+pradeep652227@users.noreply.github.com>
2024-08-12 04:03:02 +0000

    dateInJS

--- 01_basics/03_datatype-conversion.js
+++ 01_basics/03_datatype-conversion.js
@@ -1,32 +1,32 @@
-// //To Number
-// let score=33;
-// let score1="33";
-// let score2="33abc";
-// let score3=undefined;
-// let score4=null;
+//To Number
+let score=33;
+let score1="33";
+let score2="33abc";
+let score3=undefined;
+let score4=null;
 
-// let val1=Number(score1);
-// let val2=Number(score2);
+let val1=Number(score1);
+let val2=Number(score2);
 
-// /*
-// ->Non-Numeric String to Number gives NaN
-// ->undefined to Number gives NaN
-// ->null to Number gives 0
-// */
-// console.table([
-//     typeof score,
-//     typeof (val1),
-//     typeof(val2),
-//     typeof score3,
-//     typeof score4
-// ])
-// console.table([
-//     score,
-//     val1,
-//     val2,
-//     Number(score3),
-//     Number(score4)
-// ]);
+/*
+->Non-Numeric String to Number gives NaN
+->undefined to Number gives NaN
+->null to Number gives 0
+*/
+console.table([
+    typeof score,
+    typeof (val1),
+    typeof(val2),
+    typeof score3,
+    typeof score4
+])
+console.table([
+    score,
+    val1,
+    val2,
+    Number(score3),
+    Number(score4)
+]);
 
 // //To Boolean
 // let isLoggedIn=1
---
+++ 01_basics/09_DatesInJS.js
@@ -0,0 +1,32 @@
+// //Dates in Javascript
+
+let myDate=new Date();
+console.table([
+    myDate,
+    myDate.toString(),
+    myDate.toDateString(),
+    myDate.toISOString(),
+    myDate.toJSON(),
+    myDate.toLocaleString(),
+    myDate.toLocaleDateString(),
+    myDate.toLocaleTimeString()
+]);
+
+//.getXXX() methods
+let myDate1=new Date("12-04-2022 04:12:23 AM");
+console.log(myDate1);
+console.table([
+    myDate1.getTime(),
+    myDate1.toLocaleString(),
+    myDate1.getDay(),
+    myDate1.getMonth(),
+    myDate1.getDate(),
+    myDate1.getFullYear(),
+    myDate1.getMilliseconds(),
+    myDate1.getSeconds(),
+    myDate1.getMinutes(),
+    myDate1.getHours(),
+    myDate1.getUTCDate()
+]);
+
+//toLocaleString options
